d100dfb319d5639a59983b14d1d55381731cdb3d
Dhiraj Raj <dhirajraj97084@gmail.com>
2025-10-05 23:56:02 +0530

    first commit

---
+++ NaN.js
@@ -0,0 +1,11 @@
+// output is NaN 
+console.log(0/0);  
+console.log(undefined+10);
+console.log(Math.sqrt(-1));
+
+//type of NaN is - Number
+console.log(typeof NaN)
+
+// it is uniques in JavaScrip because of it is not Equat to itself
+console.log(NaN===NaN) // false
+console.log(NaN==NaN)   //false
